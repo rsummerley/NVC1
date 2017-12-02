f8661bc30919bf12cdbeeb0d9d2286950cf6804b
PHP Shift <shift@php-shift.com>
2017-12-02 19:55:28 +0000

    Add required resource link for MySQLi functions

--- 2tryconnection.php
+++ 2tryconnection.php
@@ -6,6 +6,6 @@ $hostname_tryconnection = "localhost";
 $database_tryconnection = "DVManager";
 $username_tryconnection = "root";
 $password_tryconnection = "";
-$tryconnection = mysql_connect($hostname_tryconnection, $username_tryconnection, $password_tryconnection) or trigger_error(mysqli_error($mysqli_link),E_USER_ERROR); 
+$tryconnection = mysqli_connect($hostname_tryconnection, $username_tryconnection, $password_tryconnection) or trigger_error(mysqli_error($mysqli_link),E_USER_ERROR); 
 date_default_timezone_set('America/Toronto') ;
 ?>
